893a0377d3ee3d9b4f098e0d1da6300c3cee9d9a
ryan628090@yahoo.com <ryan628090@yahoo.com>
2018-06-25 21:01:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function shout(string) {
   return string.toUpperCase()
 }
+function whisper(string){
+  console.log(string.toLowerCase())
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function sayHiToGrandma(string){
+  if(string == string.toLowerCase()){
+    return "I can't hear you"
+  }
+  else if(string == string.toUpperCase()){
+    return "YES INDEED!"
+  }
+  else(string == "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
+
